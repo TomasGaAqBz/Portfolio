3f8c310abef3b227898f97037ea47e5ab5861ec3
Tomas Aquino <tomasaquinogabi@gmail.com>
2024-02-15 15:23:58 -0300

    agregando el loader

---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className=' flex justify-center items-center'>
+        <div className='w-20 h-20 border-2 border-opacity-20 border-orange-500 border-t-orange-500 rounded-full animate-spin'>
+        </div>
+    </div>
+  )
+}
+
+export default Loader
--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,12 @@
 
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Loader from "./Loader"
 
 
 export {
     Navbar,
     Footer,
+    Loader,
 
 }
