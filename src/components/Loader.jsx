09b1449a987fde4aa2eb6e5454630583f81bf9fb
Tomas Aquino <tomasaquinogabi@gmail.com>
2024-02-16 17:02:58 -0300

    luces e isla cargan el el homePage

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,6 +4,7 @@ const Loader = () => {
   return (
     <div className=' flex justify-center items-center'>
         <div className='w-20 h-20 border-2 border-opacity-20 border-orange-500 border-t-orange-500 rounded-full animate-spin'>
+        a
         </div>
     </div>
   )
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,21 +1,56 @@
 import React from 'react'
-import { Suspense } from 'react'
-import { Canvas } from '@react-three/fiber'
+import { Canvas } from "@react-three/fiber";
+import { Suspense, useEffect, useRef, useState } from "react";
 import { Loader } from '../components'
 
+import Island from '../models/Island'
 
 
-{/* <div className=' absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
-            popup
-        </div> */}
 
 const Home = () => {
+  const adjustIslandForScreenSize = () =>{
+    let screenScale = null;
+    let screenPosition =[0, -6.5 , -150]
+    let rotation = [5 , 0 , 0]
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9 , 0.9]
+
+    } else{
+      screenScale = [1, 1 , 1]
+
+    }
+    return [screenScale, screenPosition, rotation]
+  }
+
+  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize()
+
   return (
     <section className='w-full h-screen relative'>
       <Canvas 
       className='w-full h-screen bg-transparent'
-      camera={{near:0.1, far:1000}}>
-        <Suspense fallback={<Loader/>}>
+      camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={"loading"}>
+        <directionalLight position={[4, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.6} />
+          <spotLight
+            position={[0, 50, 10]}
+            angle={0.15}
+            penumbra={1}
+            intensity={2}
+          />
+          <hemisphereLight
+            skyColor='#b1e1ff'
+            groundColor='#000000'
+            intensity={1}
+          />
+
+          <Island
+            position={islandPosition}
+            scale={islandScale}
+            rotation={islandRotation}
+          />
 
         </Suspense>
 
